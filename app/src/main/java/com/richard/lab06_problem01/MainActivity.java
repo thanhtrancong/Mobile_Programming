29dc477e3cb4f0e4a0bfe16d65f471d335e2a9fa
macbook <thanh.tc@vlu.edu.vn>
2025-10-16 10:58:27 +0700

    put code problem 01 v01

--- app/src/main/java/com/richard/lab06_problem01/MainActivity.java
+++ app/src/main/java/com/richard/lab06_problem01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.richard.lab06_problem01;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
 
+public class MainActivity extends AppCompatActivity {
+TextView show;
+Button readfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        show = findViewById(R.id.tvshow);
+        readfile = findViewById(R.id.btnreadfile);
+        readfile.setOnClickListener(v -> {
+           String data = "";
+            try {
+               InputStream iput = getAssets().open("thongtin.txt");
+               int size = iput.available();
+               byte[] buffer = new byte[size];
+                iput.read(buffer);
+                data=new String(buffer);
+            } catch (IOException e) {
+                show.setText(e.getMessage());
+            }
+            show.setText(data);
         });
     }
 }
